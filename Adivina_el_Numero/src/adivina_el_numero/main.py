37bb83d86e4066ff089f7f49bc424910bb56980a
gustavex22 <gustavex618@gmail.com>
2025-03-14 21:06:26 -0500

    [Nuevo] projectos de calculadora de Ecuaciones solo agrege una lista de Actividades que cumplira el codigo (Si tengo que usar una pinche IA para esto tambien me mato)

--- Adivina_el_Numero/src/adivina_el_numero/main.py
+++ Adivina_el_Numero/src/adivina_el_numero/main.py
@@ -1,13 +1,12 @@
 import random 
 
-Numero = random.randint(1, 100) 
+Numero =int( random.randint(1, 100)) 
 
 print("__Adivina el numero!!___")
 
-Numero_user = input("Se ha generado un numero entre 1 y 100  adivina cual es: ")
+Numero_user =int( input("Se ha generado un numero entre 1 y 100  adivina cual es: "))
 
 if Numero_user == Numero :
         print("Ganaste!!!")
-        
 else:
     print(f"Perdiste!! El numero era: {Numero}")
---
+++ Calculadora_de_Ecuaciones/src/calculadora_de_ecuaciones/main.py
@@ -0,0 +1,35 @@
+"""[Nuevo Projecto]
+ #* Calculadora de ecuaciones
+    1. como funcionaria?
+    Meter una Ecuacion cualquiera y que devuelva la posible solucion a esta
+    
+    2.Pasos
+    1.0Jerarquia de ecuaciones
+    1.1. Ecuaciones lineales [Primer test]
+    1.2 Ecuaciones cuadraticas [Segundo test]
+    
+    
+    resumen:
+  #*  El projecto de calculadora de ecuaciones debe de hacer estas cosas
+    
+    1. permitir meter ecuaciones lineales y cuadraticas
+     1.1dividir la ecuacion por jerarquias
+     1.2. resolver las divisiones
+     
+     1.3 Adaptar la formula fórmula de Bhaskar para las ecuaciones cuadraticas
+    2.Respetar la jerarquia de ecuaciones
+    3.Adaptar los metodos de resolucion de las Ecuaciones a codigo
+
+"""
+import math
+
+
+print("Calculadora de Ecuaciones")
+Ecuacion = input("Introduzca una Ecuacion Lineal")
+
+
+
+
+
+
+
