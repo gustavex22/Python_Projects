3e64946ac0d8bd60d5a34f0157395bc3a8db8fa6
gustavex22 <gustavex618@gmail.com>
2025-05-24 15:41:39 -0500

    [Pacman_Recreation][FIX] Ordenamiento de codigo ,se ordeno el codigo en funciones y archivos separados mejoran la lejibilidad del mismo

--- Pacman_Recreation/src/pacman_recreation/main.py
+++ Pacman_Recreation/src/pacman_recreation/main.py
@@ -1,7 +1,9 @@
 import pygame
 import Option
+import TecladoControler
+
 from Jugador import Personaje
-from Estructuras import Muro
+from Construc import Muro
 
 """ 
 Recreacion del juego pacman
@@ -29,16 +31,34 @@ def DibujadoEnPantalla(Ventana):
     
 
 
-def game():
-    #? creamos una ventana con las escalas de anchura y altura
+def INIT_Componente ():
+    global player,Ventana,reloj,colision,Estructura,Estructura_2,Estructura_3
+
+    #* Posicion Inicial
+    X = Option.PosicionX
+    Y= Option.PosicionY
+    
+    #* Referencia a personaje dentro de player
+    player =  Personaje(X,Y)#? Ademas inicializa el personaje en la posicion inicial
+     #? creamos una ventana con las escalas de anchura y altura
     Ventana =pygame.display.set_mode((Option.Ancho , Option.Altura))#variable para abreviar el codigo
     
     pygame.display.set_caption(Option.Ventana_name)#?Se establece el nombre de la ventana
     
     reloj = pygame.time.Clock()#?Se establece el reloj para abreviar el codigo
     
-    Run=True
+    colision = player.forma.colliderect #?establecer colision para abreviar codigo
+    Estructura = Muro(200,200)
+    Estructura_2 =Muro(400,400)
+    Estructura_3 =Muro(600,200)
     
+    
+def game():
+    #* Inicializar componentes
+    INIT_Componente()
+    
+    Run=True
+
     while(Run):
         #*Codigo del juego aqui
         
@@ -55,63 +75,30 @@ def game():
         PX = 0
         PY = 0
         Velocidad = Option.Velocity
-        
-        #? Sentencias If de movimiento
-        
-        if Option.M_Izquierda == True:
-            PX -=  Velocidad
-        if Option.M_Derecha == True:
-            PX += Velocidad
-        if Option.M_Arriba == True:
-            PY -= Velocidad
-        if Option.M_Atras == True:
-            PY  += Velocidad
-        
-        player.Movimiento(PX,PY) #?Actualiza el movimiento del jugador
 
-       
-        colision = player.forma.colliderect
+        #? Sentencias If de movimiento
+        PX,PY = TecladoControler.Update_Movement(PX,PY,Option,Velocidad,player)
+
+        
         #?Colisiones
+        
         if colision(Estructura.forma ) or colision(Estructura_2.forma ) or colision(Estructura_3.forma ) :
-            
             #*Colision detectada
-            print("Colision Detectada")
-            player.Movimiento(-PX,-PY) #? Si hay colision se mueve en la direccion contraria
-           
-        else:
-            print("Colision No Detectada")
             
+            player.Movimiento(-PX,-PY) #? Si hay colision se mueve en la direccion contraria
+
+
         DibujadoEnPantalla(Ventana) #! Esta funcion solo ordena el orden en que se renderiza ciertos objetos
 
-        
-        #?bucle para reconocer que teclas se han presionado
-        for event in pygame.event.get():
-            
+
+        for event in pygame.event.get():#?bucle para reconocer que teclas se han presionado
+
             #*Evento salir
-            if event.type == pygame.QUIT:
-                Run = False
-                
+            Run =TecladoControler.Identifier_Exit(Run,event)
+            
             #*Eventos de movimientos al tocar una tecla
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    Option.M_Izquierda = True
-                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    Option.M_Derecha = True
-                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    Option.M_Atras = True
-                if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    Option.M_Arriba = True
-                #! Al dejar de tocar una tecla ,(Por si acaso, no es necesario esta parte del codigo )
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    Option.M_Izquierda = False
-                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    Option.M_Derecha = False
-                if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    Option.M_Arriba = False
-                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    Option.M_Atras = False
-        
+            TecladoControler.Identifier_Key(event,Option)
+
         pygame.display.update(); #?Actualizar la pantalla
         
 
@@ -119,17 +106,6 @@ def game():
 #*Ejecucion principal
 
 pygame.init()
-
-#* Posicion Inicial
-X = Option.PosicionX
-Y= Option.PosicionY
-
-#* Referencia a personaje dentro de player
-player =  Personaje(X,Y)#? Ademas inicializa el personaje en la posicion inicial
-
-Estructura = Muro(200,200)
-Estructura_2 =Muro(400,400)
-Estructura_3 =Muro(600,200)
 
 #* Juego principal
 game()
